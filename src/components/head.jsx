9a8f0c42afdfc26f94e00c04f94140bc177f8742
ota-takeru <otataker@gmail.com>
2023-03-07 10:42:38 +0900

    20230307

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -2,16 +2,13 @@ import Head from 'next/head'
 import React from 'react'
 
 const CustomHead = ( props ) => {
+  console.log(props.descriptions)
   return (
     <Head>
       <title>{props.title}</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="description" content={props.descriptions} />
       <meta name="keywords" content={props.keywords} />
-      <meta name="author" content="Jesse" />
-      <meta name="robots" content="index, follow" />
-      <meta name="googlebot" content="index, follow" />
-      <meta name="google" content="nositelinkssearchbox" />
     </Head>
   )
 
