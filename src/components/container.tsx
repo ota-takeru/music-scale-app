076aa8b4a6aa9e041c5ee300ca1409b64112dabc
ota-takeru <otataker@gmail.com>
2026-07-19 23:08:10 +0900

    Center search content and show instruments

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -6,7 +6,7 @@ interface ContainerProps {
 
 const Container: React.FC<ContainerProps> = React.memo(({ children }) => {
   return (
-    <div className="w-full max-w-6xl mx-auto px-10 rounded-lg md:px-0 min-[1500px]:ml-64 min-[1500px]:mx-0">
+    <div className="w-full max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 rounded-lg">
       {children}
     </div>
   )
